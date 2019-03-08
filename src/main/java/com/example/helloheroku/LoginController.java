150ab08c5299be5c5b3a77c74b070ee1ca37c9aa
unknown <you@example.comfubin22721@icloud.com>
2019-03-08 19:01:20 +0900

    make it better

--- src/main/java/com/example/helloheroku/LoginController.java
+++ src/main/java/com/example/helloheroku/LoginController.java
@@ -1,9 +1,5 @@
 package com.example.helloheroku;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,26 +8,51 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import jp.co.agrex.aps2jni.Aps2Coder;
 import jp.co.agrex.aps2jni.Aps2Controller;
 import jp.co.agrex.aps2jni.Aps2Exception;
-import jp.co.agrex.aps2jni.Aps2IOException;
-import jp.co.agrex.aps2jni.Aps2IllegalArgumentException;
-import jp.co.agrex.aps2jni.Aps2IllegalEnvironmentException;
-import jp.co.agrex.aps2jni.Aps2IllegalStateException;
 import jp.co.agrex.aps2jni.Aps2MasterData;
-import jp.co.agrex.aps2jni.Aps2ProcessException;
 import jp.co.agrex.aps2jni.Aps2RuntimeException;
 import jp.co.agrex.aps2jni.Aps2StdCoderResult;
-import jp.co.agrex.aps2jni.Aps2TimeOverException;
+
 
 @Controller
 public class LoginController {
  @RequestMapping(value = "/login", method = RequestMethod.POST)
  public String login(Model model) {
-	 System.out.println(System.getProperty("java.library.path"));
-	 System.out.println( "===== Console Test START =====" );
-		Aps2JNICoderTest test = new Aps2JNICoderTest();
-		test.start();
-		System.out.println( "===== Console Test END =====" );
-		
-		 return null;
+	 
+	 	String iniPath = "../ini/aps2env.ini";
+	 	
+	 	String target = "東京都立川市";
+	 	
+	 	try {
+	 		System.out.println( "  JAVA.VERSION :[" + System.getProperty( "java.version" ) + "]" );
+	 		System.out.println( "  JAVA.VENDOR  :[" + System.getProperty( "java.vendor" ) + "]" );
+	 		System.out.println( "  JAVA.HOME    :[" + System.getProperty( "java.home" ) + "]" );
+		 	
+		 	Aps2Coder _cd = new Aps2Coder();
+	 		
+	 		Aps2Controller cont = new Aps2Controller();
+	 		
+	 		cont.prologue( iniPath );
+		 	
+		 	Aps2StdCoderResult _stdresult = _cd.standardCoder(_cd.FUNC_MODE_ADDRESS,
+					  _cd.FUNC_OPTION_NEW_MASTER, target, "", "");
+		 	
+		 	if(_stdresult.getNewMasterNo() != 0 &&
+					_stdresult.getNewCodeFlag() == _stdresult.NEW_CODE_FLAG_GENZON) {
+		 		
+				Aps2MasterData _latest = _stdresult.getMasterData();
+				
+				model.addAttribute("adress_code", _latest.getCode());
+				
+		 	}
+		 	
+
+		} catch( Aps2Exception e ){
+			e.printStackTrace();
+		} catch( Aps2RuntimeException re ){
+			re.printStackTrace();
+		}
+	 
+//	 model.addAttribute("adress_code", "123-4567");//do　not delete
+	 	return "top";
  }
 }
